072d675330e8ff988f290977334f130adf65532d
SviatoslavZenkevich <sviatoslav.zenkevich@gmail.com>
2023-01-14 23:45:10 +0300

    Create Dataclass

---
+++ app/src/main/java/com/example/cityweather/DayItem.kt
@@ -0,0 +1,9 @@
+package com.example.cityweather
+
+data class DayItem(
+    val city: String,
+    val date: String,
+    val condition: String,
+    val imageUrl: String,
+
+)
---
+++ app/src/main/java/com/example/cityweather/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.cityweather
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData <String> ()
+    val liveDataList = MutableLiveData <List<String>> ()
+
+}
