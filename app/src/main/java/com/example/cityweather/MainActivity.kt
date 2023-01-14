04118d9646857c2650a0ace572d6ac5b44797740
SviatoslavZenkevich <sviatoslav.zenkevich@gmail.com>
2023-01-14 23:05:20 +0300

    Creat MainFragment

---
+++ app/src/main/java/com/example/cityweather/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.cityweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cityweather.Fragments.MainFragment
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
+    }
+}
+
+
