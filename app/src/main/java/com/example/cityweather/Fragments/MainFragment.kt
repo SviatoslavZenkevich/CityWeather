a2d6b306e7ead4b4b529ec833e0bfa3a6bbfd134
SviatoslavZenkevich <sviatoslav.zenkevich@gmail.com>
2023-01-15 21:19:05 +0300

    A lot of changes, but stop working

--- app/src/main/java/com/example/cityweather/Fragments/MainFragment.kt
+++ app/src/main/java/com/example/cityweather/Fragments/MainFragment.kt
@@ -1,27 +1,104 @@
 package com.example.cityweather.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cityweather.R
+import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.cityweather.WeatherModel
 import com.example.cityweather.databinding.FragmentMainBinding
+import org.json.JSONObject
+
+const val API_KEY = "db9a768016c3403cb03162546231501"
+private lateinit var binding: FragmentMainBinding
+
 
 class MainFragment : Fragment() {
-    private lateinit var binding: FragmentMainBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requestWeatherData("Berlin")
     }
+
+    private fun requestWeatherData(city: String){
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                    result -> parseWeatherData(result)
+            },
+            {
+                    error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
+    }
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel>{
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name =  mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until 4){
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("text"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("icon"),
+                day.getJSONObject("day").getString("maxtemp_c"),
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel){
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("icon"),
+            mainObject.getJSONObject("current").getString("temp_c")
+        )
+
+        Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.date}")
+        Log.d("MyLog", "Temp: ${item.currentTemp}")
+    }
+
+
     companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
